1bdf8dda82d9e71fd4889bfa1a371391535d8b5c
Dwi Hardiman <dwihardyman@gmail.com>
2016-06-06 22:45:53 +0700

    Show and Index Pages

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,7 +1,8 @@
 class PicsController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @pics = Pic.all
   end
 
   def new
@@ -10,12 +11,25 @@ class PicsController < ApplicationController
 
   def create
     @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: "Awesome! it was posted!"
+    else
+      render 'new'
+    end
+  end
+
+  def show
   end
 
   private
 
   def pic_params
-    params.require(:pic).permit(:title, :desription)
+    params.require(:pic).permit(:title, :description)
+  end
+
+  def find_pic
+    @pic = Pic.find(params[:id])
   end
 
 end
